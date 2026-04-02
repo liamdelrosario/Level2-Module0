d900cc8656207dff5837386f3d200cab5c0630bb
league <league@192.168.1.135>
2026-04-01 19:57:58 -0700

    are array and list the same

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -26,12 +26,20 @@ public class _00_ArrayCheatSheet {
 		Random rand = new Random();
 		int[] fiftyInt = new int[50];
 		//6. make an array of 50 integers
+	//	int smallestValue = 500;
+		int largestValue = 0;
 		for(int i=0; i<fiftyInt.length; i++) {
 		fiftyInt[i]=rand.nextInt(500);
+	//	if(fiftyInt[i]=<smallestValue) {
+	//		smallestValue=fiftyInt[i];
+	//}
+		if(fiftyInt[i]>=largestValue) {
+			largestValue = fiftyInt[i];
 		}
-	
+		}
+		System.out.println(largestValue);
 		//7. use a for loop to make every value of the integer array a random number
-		
+
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
